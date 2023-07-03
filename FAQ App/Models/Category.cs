ed35accfbaa4c3b6a2dc64a985dd77d9cd66e8aa
Manjeet Kumar <manjeet.kmr18@gmail.com>
2023-07-03 13:54:11 -0400

    models added

---
+++ FAQ App/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace FAQ_App.Models
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<FAQ> FAQs { get; set; }
+    }
+}
--- FAQ App/Models/FAQ.cs
+++ FAQ App/Models/FAQ.cs
@@ -2,6 +2,13 @@
 {
     public class FAQ
     {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
 
+        public string TopicId { get; set; }
+        public string CategoryId { get; set; }
+        public virtual Topic Topic { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
